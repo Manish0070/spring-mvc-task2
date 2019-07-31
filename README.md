746abde8a60f24891ab4a76bba877db85df5553e
Manish <manish.perumalla@cgi.com>
2019-07-31 13:57:46 +0530

    Task2 Spring MVC

---
+++ README.md
@@ -0,0 +1,5 @@
+This Project Contains Following Task.
+
+UserController will create user object and will send user object to View Page (index) .
+
+View Page Should display “Welcome <user> to stackroute”.
---
+++ src/main/java/com/stackroute/controller/User.java
@@ -0,0 +1,34 @@
+package com.stackroute.controller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+    private String name;
+    private String password;
+    public User(String name, String password) {     //Parameterized constructor
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {     //Default constructor
+    }
+
+    public String getName() {       //getter()
+        return name;
+    }
+
+    public void setName(String name) {      //setter()
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
